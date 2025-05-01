c6dfec0b8e97e82ce7842c863d389c829787309b
harimaurya111 <139848015+harimaurya111@users.noreply.github.com>
2025-05-01 16:28:16 +0530

    fixed bugs

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../redux/features/cart/CartSlice";
 import authReducer from "./features/auth/authSlice"
-import authApi from "./features/auth/authApi";
+import authApi from "../../src/redux/features/auth/authapi.js";
 
 export const store = configureStore({
   reducer: {
